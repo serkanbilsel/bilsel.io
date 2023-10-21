b3c79348dba39388a13774059a0e89c0b485a4cb
serkanbilsel <serkanbilsel@gmail.com>
2023-10-22 00:32:58 +0300

    22.10 Last UpdateDesing
    Desing Ux Ui
    Responsive work

--- serkanbilsel/Program.cs
+++ serkanbilsel/Program.cs
@@ -23,14 +23,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 app.UseAuthorization();
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "areas",
             pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
           );
        
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
